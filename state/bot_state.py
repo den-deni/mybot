ff79d4b92f500033ed63edef90dba27011f3f574
denis <git@github.com:DenzelWebLab/test.git>
2025-02-10 23:32:02 +0200

    add requirements.txt

--- state/bot_state.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-
-class BotState(StatesGroup):
-    youtube = State()
-    city = State()
-    coins = State()
-    token = State()
-    text = State()
--- utils/audio_text.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-
-
-def text_to_speech(text):
-    save_path = "./static/audio/voice.mp3"
-    tts = gTTS(text=text, lang='uk')
-    tts.save(savefile=save_path)
-    return save_path
